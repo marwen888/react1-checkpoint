b2733d951c6d383d22191333466290a7965ca7a9
marwen888 <mejerbi.marouen@gmail.com>
2021-09-13 15:50:01 +0100

    this is react

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
-import logo from './logo.svg';
+//import logo from './logo.svg';
+import React from 'react';
 import './App.css';
-
+import 'bootstrap';
+import ReactBootstrap, {Container, Jumbotron, Button, Row, Col, Grid, Panel,Form, FormGroup, FormLabel, FormText,FormControl} from 'react-bootstrap'
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+ 
+  <Form>
+  <Form.Group as={Row} className="mb-3" controlId="formPlaintextEmail">
+    <Form.Label column sm="2">
+      Email
+    </Form.Label>
+    <Col sm="10">
+      <Form.Control plaintext readOnly defaultValue="email@example.com" />
+    </Col>
+  </Form.Group>
+
+  <Form.Group as={Row} className="mb-3" controlId="formPlaintextPassword">
+    <Form.Label column sm="2">
+      Password
+    </Form.Label>
+    <Col sm="10">
+      <Form.Control type="password" placeholder="Password" />
+    </Col>
+  </Form.Group>
+</Form>
     </div>
   );
 }
